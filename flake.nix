94abf92016df25d2f696d5ec8c7424a85c07d9b4
Tristan Cacqueray <tdecacqu@redhat.com>
2023-07-06 14:01:59 +0000

    Add build-depends and module scaffold

--- flake.nix
+++ flake.nix
@@ -10,6 +10,29 @@
 
       haskellExtend = hpFinal: hpPrev: {
         progcon = hpPrev.callCabal2nix "progcon" "${self}/progcon-hs" { };
+        # Bump requested at https://github.com/ekmett/gl/issues/24
+        gl = pkgs.haskell.lib.doJailbreak hpPrev.gl;
+        massiv = pkgs.haskell.lib.doJailbreak
+          (pkgs.haskell.lib.overrideCabal hpPrev.massiv {
+            patches = [
+              (pkgs.fetchpatch {
+                name = "p1";
+                url =
+                  "https://github.com/lehins/massiv/commit/040afd7d57f90b5fe1090654005f9ca506ce3bd9.patch";
+                sha256 = "sha256-j/BvGL/Bd+dknbZ4FThPLOWvelUUtrQiQ6tcRHzEnUY=";
+                stripLen = 1;
+              })
+              (pkgs.fetchpatch {
+                name = "p2";
+                url =
+                  "https://github.com/lehins/massiv/commit/9eb03ebe03f658a3401ec2a26a55be175eeb390c.patch";
+                sha256 = "sha256-tj5taGaoKzNrKtwaULur+SAlRT/powjp4TSdxwZMFF4=";
+                stripLen = 1;
+              })
+            ];
+          });
+        dear-imgui = pkgs.haskell.lib.doJailbreak hpPrev.dear-imgui;
+        pvar = pkgs.haskell.lib.dontCheck hpPrev.pvar;
       };
       hsPkgs = pkgs.hspkgs.extend haskellExtend;
 
