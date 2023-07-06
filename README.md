4a921d496a443ad9354198131c64bed657815749
Tristan Cacqueray <tdecacqu@redhat.com>
2023-07-06 14:48:58 +0000

    Add SDL boilerplate

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # JKRT-2023
 
+```ShellSession
+# Hot reload the interface
+ghcid --command "cabal repl progcon" -W --test ProgCon.GUI.main
+```
+
 ## nix
 
 ```ShellSession
@@ -8,4 +13,24 @@ nix develop -c ghcid --command "cabal repl progcon"
 
 # Run the tool
 nix run
+```
+
+## SDL_GL_CreateContext error and nix
+
+If the program fails with:
+
+```ShellSession
+SDLCallFailed {sdlExceptionCaller = "SDL.Video.glCreateContext", sdlFunction = "SDL_GL_CreateContext", sdlExceptionError = "Invalid window"}
+```
+
+You need to use `nixGL`. To install the right version (see hspkgs input to match the nixpkgs pin):
+
+```ShellSession
+nix profile install --override-input nixpkgs github:NixOS/nixpkgs/22c5bd85d8478e24874ff2b80875506f5c3711a6 --impure github:guibou/nixGL
+```
+
+Then run:
+
+```ShellSession
+nixGL ghcid ...
 ```
--- progcon-hs/src/ProgCon/GUI.hs
+++ progcon-hs/src/ProgCon/GUI.hs
@@ -1,1 +1,76 @@
 module ProgCon.GUI where
+
+import Control.Monad.Managed (managed, managed_, runManaged)
+import Data.Vector.Storable qualified as SV
+import Data.Vector.Storable.Mutable qualified as V
+import DearImGui.OpenGL3 qualified
+import DearImGui.SDL qualified
+import DearImGui.SDL.OpenGL qualified
+import Graphics.GL qualified as GL
+import System.Environment (getArgs)
+import Witch (from)
+
+import DearImGui
+import RIO
+import SDL hiding (Texture)
+
+usage :: IO ()
+usage =
+    getArgs >>= \case
+        [] -> pure ()
+        _ -> error "usage: progcon-gui"
+
+main :: IO ()
+main = do
+    usage
+
+    initializeAll
+
+    let keyHandler event
+          | isQuit = pure True
+          | otherwise = pure False
+         where
+            keyCode = case eventPayload event of
+                KeyboardEvent ke | ke.keyboardEventKeyMotion == Pressed -> Just ke.keyboardEventKeysym.keysymScancode
+                _ -> Nothing
+            isQuit =
+                SDL.eventPayload event == SDL.QuitEvent
+                    || keyCode == Just ScancodeEscape
+
+    let withTexture cb = do
+            cb ()
+
+    mainGUI keyHandler withTexture \() -> withFullscreen do
+        text "progcon demo"
+        DearImGui.plotLines "samples" [sin (x / 10) | x <- [0..64]]
+
+-- sdl bootstrap adapted from the dear-imgui readme.
+mainGUI :: (Event -> IO Bool) -> _ -> (_ -> IO ()) -> IO ()
+mainGUI eventHandler withTextures renderUI = do
+    runManaged do
+        window <- do
+            let title = "simple-dsp-demo"
+            let config = defaultWindow{windowGraphicsContext = OpenGLContext defaultOpenGL}
+            managed $ bracket (createWindow title config) destroyWindow
+        glContext <- managed $ bracket (glCreateContext window) glDeleteContext
+        _ <- managed $ bracket createContext destroyContext
+        _ <- managed_ $ bracket_ (DearImGui.SDL.OpenGL.sdl2InitForOpenGL window glContext) DearImGui.SDL.sdl2Shutdown
+        _ <- managed_ $ bracket_ DearImGui.OpenGL3.openGL3Init DearImGui.OpenGL3.openGL3Shutdown
+
+        liftIO $ withTextures \textures -> mainLoop window (renderUI textures) eventHandler
+
+mainLoop :: Window -> IO () -> (Event -> IO Bool) -> IO ()
+mainLoop window renderUI eventHandler = unlessQuit do
+    DearImGui.OpenGL3.openGL3NewFrame
+    DearImGui.SDL.sdl2NewFrame
+    DearImGui.newFrame
+    renderUI
+    GL.glClear GL.GL_COLOR_BUFFER_BIT
+    DearImGui.render
+    DearImGui.OpenGL3.openGL3RenderDrawData =<< getDrawData
+    SDL.glSwapWindow window
+    mainLoop window renderUI eventHandler
+  where
+    unlessQuit action = do
+        shouldQuit <- traverse eventHandler =<< DearImGui.SDL.pollEventsWithImGui
+        unless (or shouldQuit) action
