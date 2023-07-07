e7700b8f8fcd81199d11c95263b3e618bcaf9149
Tristan Cacqueray <tdecacqu@redhat.com>
2023-07-07 12:36:12 +0000

    Add parser

--- progcon-hs/src/ProgCon/Syntax.hs
+++ progcon-hs/src/ProgCon/Syntax.hs
@@ -1,1 +1,30 @@
 module ProgCon.Syntax where
+
+data Attendees = Attendees
+    { attendeesTastes :: [Int]
+    , attendeesX :: Int
+    , attendeesY :: Int
+    }
+    deriving (Show, Eq, Ord)
+
+data Problem = Problem
+    { problemStageHeight :: Int
+    , problemStageWidth :: Int
+    , problemMusicians :: [Int]
+    , problemRoomHeight :: Int
+    , problemAttendees :: [Attendees]
+    , problemStageBottomLeft :: [Int]
+    , problemRoomWidth :: Int
+    }
+    deriving (Show, Eq, Ord)
+
+data Solution = Solution
+    { solutionPlacements :: [Placements]
+    }
+    deriving (Show, Eq, Ord)
+
+data Placements = Placements
+    { placementsX :: Int
+    , placementsY :: Int
+    }
+    deriving (Show, Eq, Ord)
