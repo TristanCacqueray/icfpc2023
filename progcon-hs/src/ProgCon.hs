2ece1a99c46d4ce32267cdafcdadf0c93970ea01
Tristan Cacqueray <tdecacqu@redhat.com>
2023-07-07 12:36:12 +0000

    Add parser

--- progcon-hs/src/ProgCon.hs
+++ progcon-hs/src/ProgCon.hs
@@ -1,4 +1,13 @@
 module ProgCon (someFunc) where
 
+import ProgCon.Parser
+import ProgCon.Syntax
+
+solve :: Problem -> Solution
+solve _ = Solution []
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do
+  problem <- loadProblem "../problems/problem-1.json"
+  putStrLn $ "musicians: "<> show (length problem.problemMusicians)
+  writeSolution (solve problem)
