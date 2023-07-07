7a2c80eec2eb0195b6bd0106e86d34a2e62caa53
Tristan Cacqueray <tdecacqu@redhat.com>
2023-07-07 15:24:56 +0000

    Add initial eval

--- src/ProgCon/Eval.hs
+++ src/ProgCon/Eval.hs
@@ -1,1 +1,22 @@
 module ProgCon.Eval where
+
+import Data.Vector.Unboxed qualified as UV
+import Data.Vector.Unboxed ((!))
+
+import ProgCon.Syntax
+
+attendeeHappiness :: UV.Vector Int -> Solution -> Attendee -> Float
+attendeeHappiness instruments solution attendee = UV.sum $ UV.imap musicianImpact solution.solutionPlacements
+  where
+    musicianImpact :: Int -> (Float, Float) -> Float
+    musicianImpact musician (px, py)
+      | isBlocked = 0
+      | otherwise = 1_000_000 * taste / (distance ** 2)
+     where
+       instrument = instruments ! musician
+       taste = attendee.attendeeTastes ! instrument
+       distance = sqrt ((attendee.attendeeX - px) ** 2 + (attendee.attendeeY - py) ** 2)
+       isBlocked = px == py -- TODO
+
+score :: Problem -> Solution -> Float
+score problem solution = sum $ map (attendeeHappiness problem.problemMusicians solution) problem.problemAttendees
