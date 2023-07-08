ac0539be32224c2627a999cf372db55b54794c75
Tristan Cacqueray <tdecacqu@redhat.com>
2023-07-08 02:20:48 +0000

    Replace imap with a pre-built indexed position

--- src/ProgCon/Eval.hs
+++ src/ProgCon/Eval.hs
@@ -5,11 +5,11 @@ import Data.Vector.Unboxed ((!))
 
 import ProgCon.Syntax
 
-attendeeHappiness :: UV.Vector Int -> Solution -> Attendee -> Int
-attendeeHappiness instruments solution attendee = UV.sum $ UV.imap musicianImpact solution.solutionPlacements
+attendeeHappiness :: UV.Vector Int -> UV.Vector (Int, Float, Float) -> Attendee -> Int
+attendeeHappiness instruments solution attendee = UV.sum $ UV.map musicianImpact solution
   where
-    musicianImpact :: Int -> (Float, Float) -> Int
-    musicianImpact !musician placement
+    musicianImpact :: (Int, Float, Float) -> Int
+    musicianImpact (musician, px, py)
       | isBlocked = 0
       | otherwise = ceiling $! (1_000_000 * taste) / distance
      where
@@ -18,21 +18,20 @@ attendeeHappiness instruments solution attendee = UV.sum $ UV.imap musicianImpac
        -- the attendee taste for this instrument
        taste = attendee.attendeeTastes ! instrument
        -- the distance between the attendee and the musician
-       distance = calcDistance attendee placement
+       distance = calcDistance attendee (px, py)
        -- is the musician blocked by another musician?
-       isBlocked = UV.or (UV.imap checkBlocked solution.solutionPlacements)
-       checkBlocked :: Int -> (Float, Float) -> Bool
-       checkBlocked otherMusician otherPlacement = otherInstrument /= instrument && otherDistance < distance && isCrossed
+       isBlocked = UV.any checkBlocked solution
+       checkBlocked :: (Int, Float, Float) -> Bool
+       checkBlocked (otherMusician, ox, oy) = otherInstrument /= instrument && otherDistance < distance && isCrossed
         where
-          otherDistance = calcDistance attendee otherPlacement
+          otherDistance = calcDistance attendee (ox, oy)
           otherInstrument = instruments ! otherMusician
           isCrossed =
             -- See: https://mathworld.wolfram.com/Circle-LineIntersection.html
             let
               radius = 5
-              (px, py) = otherPlacement
               (x1, y1) = (attendee.attendeeX - px, attendee.attendeeY - py)
-              (x2, y2) = (fst placement - px, snd placement - py)
+              (x2, y2) = (px - ox, py - oy)
               d = x1 * y2 - x2 * y1
               discriment = radius ** 2 * otherDistance - d ** 2
              in discriment >= 0
@@ -41,4 +40,6 @@ calcDistance :: Attendee -> (Float, Float) -> Float
 calcDistance attendee (px, py) = (attendee.attendeeX - px) ** 2 + (attendee.attendeeY - py) ** 2
 
 scoreHappiness :: Problem -> Solution -> Int
-scoreHappiness problem solution = sum $ map (attendeeHappiness problem.problemMusicians solution) problem.problemAttendees
+scoreHappiness problem solution = sum $ map (attendeeHappiness problem.problemMusicians indexedPlacements) problem.problemAttendees
+  where
+    indexedPlacements = UV.imap (\idx (x, y) -> (idx, x, y)) solution.solutionPlacements
