38610c52824f7e522107c722ece24a1cb8ccaceb
Jens Petersen <juhpetersen@gmail.com>
2023-07-09 03:48:27 +0800

    [broken encoding!] Submit refactor

--- src/ProgCon/Submit.hs
+++ src/ProgCon/Submit.hs
@@ -2,15 +2,18 @@ module ProgCon.Submit where
 
 import Control.Concurrent (threadDelay)
 import Data.Aeson
-import Data.ByteString qualified as BS
+--import Data.ByteString qualified as BS
 import Data.ByteString.Lazy qualified as BSL
-import Data.Text (Text, pack, unpack)
-import Data.Text.Encoding (decodeUtf8, encodeUtf8)
-import Network.HTTP.Client
-import Network.HTTP.Client.TLS (newTlsManager)
-import Network.HTTP.Types.Status (statusCode)
-import System.Environment
-import System.Time.Extra (sleep)
+import Data.Text qualified as T
+import Data.Text.IO qualified as T
+import Data.Text (Text)
+import Data.Text.Encoding (decodeUtf8, {-encodeUtf8-})
+import Network.HTTP.Query
+import Network.HTTP.Simple
+
+import Network.HTTP.Types.Status --(statusCode)
+--import System.Environment
+-- import System.Time.Extra (sleep)
 
 import Control.Monad (unless)
 import Data.Foldable (traverse_)
@@ -19,65 +22,41 @@ import ProgCon.Solve (toSolution)
 import ProgCon.Syntax
 import System.Directory (doesFileExist)
 
+import ProgCon.API (accessAPI)
+
 newtype SubmitID = SubmitID Text
     deriving newtype (FromJSON)
     deriving (Show)
 
 submit :: ProblemID -> Solution -> IO (Maybe SubmitID)
 submit pid solution = do
-    let obj = object ["problem_id" .= pid, "contents" .= decodeUtf8 (BSL.toStrict $ encode solution)]
-    token <- getEnv "ICFP_TOKEN"
-    manager <- newTlsManager
-    initialRequest <- parseRequest "https://api.icfpcontest.com/submission"
-    let request =
-            initialRequest
-                { method = "POST"
-                , requestBody = RequestBodyLBS $ encode obj
-                , requestHeaders =
-                    [ ("Content-Type", "application/json")
-                    , ("Authorization", "Bearer " <> encodeUtf8 (pack token))
-                    ]
-                }
-    response <- httpLbs request manager
-    if statusCode (responseStatus response) == 201
-        then pure $ decode (responseBody response)
-        else do
-            putStrLn $ "The status code was: " ++ show (statusCode $ responseStatus response)
-            print $ responseBody response
-            sleep 1 -- server often fails
-            submit pid solution
+    let obj = object ["problem_id" .= pid,
+                      "contents" .= decodeUtf8 (BSL.toStrict $ encode solution)]
+        settings =
+          setRequestMethod "POST" .
+          setRequestBodyLBS (encode obj)
+--    error $ show (encode obj)
+    accessAPI "submission" [] settings status201
 
-getInfo :: SubmitID -> IO BS.ByteString
-getInfo (SubmitID sid) = do
-    token <- getEnv "ICFP_TOKEN"
-    manager <- newTlsManager
-    initialRequest <- parseRequest $ "https://api.icfpcontest.com/submission?submission_id=" <> unpack sid
-    let request =
-            initialRequest
-                { method = "GET"
-                , requestHeaders =
-                    [ ("Content-Type", "application/json")
-                    , ("Authorization", "Bearer " <> encodeUtf8 (pack token))
-                    ]
-                }
-    response <- httpLbs request manager
-    pure $ BSL.toStrict $ responseBody response
+getInfo :: SubmitID -> IO (Maybe Text)
+getInfo (SubmitID sid) =
+  accessAPI "submission" (makeKey "submission_id" $ T.unpack sid) id status200
 
 waitFor :: SubmitID -> IO ()
 waitFor sid = do
-    resp <- getInfo sid
-    if
-            | "Processing" `BS.isInfixOf` resp -> do
+    mresp <- getInfo sid
+    case mresp of
+      Just resp
+            | "Processing" `T.isInfixOf` resp -> do
                 putStrLn "Processing..."
                 threadDelay 5_000_000
                 waitFor sid
-            | "Success" `BS.isInfixOf` resp -> do
-                BS.putStr $ BS.take 141 resp
-                BS.putStr "\n"
+            | "Success" `T.isInfixOf` resp -> do
+                T.putStrLn $ T.take 141 resp
             | otherwise -> do
-                BS.putStr "!! FAILURE: "
-                BS.putStr $ BS.take 207 resp
-                BS.putStr "\n"
+                T.putStr "!! FAILURE: "
+                T.putStrLn $ T.take 207 resp
+      Nothing -> putStrLn $ "getInfo failed for " ++ show sid
 
 submitOne :: Bool -> ProblemID -> IO ()
 submitOne lenient pid = do
