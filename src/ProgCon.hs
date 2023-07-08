6d54c4b67cd78265281ce8511236cd887601b68b
Tristan Cacqueray <tdecacqu@redhat.com>
2023-07-08 00:49:51 +0000

    Add save command to compute all the solutions in parallel

--- src/ProgCon.hs
+++ src/ProgCon.hs
@@ -1,50 +1,80 @@
 module ProgCon (main) where
 
-import System.Environment (getArgs)
 import Control.Monad
-
+import Data.Aeson qualified as Aeson
 import Data.Vector.Unboxed qualified as UV
+import Say
+import System.Directory (doesFileExist)
+import System.Environment (getArgs)
+import System.FilePath (takeBaseName)
 
+import Control.Concurrent.Async (mapConcurrently_)
 import ProgCon.Eval
-import ProgCon.Parser
-import ProgCon.Syntax
 import ProgCon.GUI
+import ProgCon.Parser
 import ProgCon.Solve
+import ProgCon.Syntax
 
 mainCheck :: FilePath -> FilePath -> IO Int
 mainCheck problemPath solutionPath = do
-  problem <- loadJSON @Problem problemPath
-  solution <- loadJSON @Solution solutionPath
-  pure (scoreHappiness problem solution)
+    problem <- loadJSON @Problem problemPath
+    solution <- loadJSON @Solution solutionPath
+    pure (scoreHappiness problem solution)
 
 mainSolve :: FilePath -> IO ()
 mainSolve problemPath = do
-  problem <- loadJSON @Problem problemPath
-  solution <- solve problem
-  writeSolution solution
+    problem <- loadJSON @Problem problemPath
+    (score, solution) <- solve (takeBaseName problemPath) problem
+    putStrLn $ "Score: " <> show score
+    writeSolution solution
+
+saveSolve :: FilePath -> IO ()
+saveSolve problemPath = do
+    sayString $ name <> ": starting..."
+    prevScore <- do
+        hasScore <- doesFileExist scorePath
+        if hasScore
+            then loadJSON @Int scorePath
+            else pure minBound
+    problem <- loadJSON @Problem problemPath
+    (score, solution) <- solve name problem
+    if score > prevScore
+        then do
+            sayString $ name <> ": new highscore: " <> show score
+            Aeson.encodeFile scorePath score
+            let resultPath = problemPath <> ".solution.json"
+            Aeson.encodeFile resultPath solution
+        else do
+            sayString $ name <> ": same score: " <> show score
+  where
+    name = takeBaseName problemPath
+    scorePath = problemPath <> ".score"
 
 mainRender :: FilePath -> IO ()
 mainRender problemPath = do
-  problem <- loadJSON @Problem problemPath
-  putStrLn $ "musicians: "<> show (UV.length problem.problemMusicians)
-  putStrLn $ "room: " <> show (problem.problemRoomWidth, problem.problemRoomHeight)
-  putStrLn $ "stage: " <> show (problem.problemStageWidth, problem.problemStageHeight)
-  putStrLn $ "stagePos: " <> show problem.problemStageBottomLeft
-  solution <- solve problem
-  renderProblem problem solution
+    problem <- loadJSON @Problem problemPath
+    putStrLn $ "musicians: " <> show (UV.length problem.problemMusicians)
+    putStrLn $ "room: " <> show (problem.problemRoomWidth, problem.problemRoomHeight)
+    putStrLn $ "stage: " <> show (problem.problemStageWidth, problem.problemStageHeight)
+    putStrLn $ "stagePos: " <> show problem.problemStageBottomLeft
+    (score, solution) <- solve (takeBaseName problemPath) problem
+    putStrLn $ "Score: " <> show score
+    writeSolution solution
+    renderProblem problem solution
 
 mainTest :: IO ()
 mainTest = do
-  res <- mainCheck "./problems/problem-spec.json" "./problems/solution-spec.json"
-  unless (res == 5343) do
-    error $ "Invalid spec score, expected 5343, got: "<> show res
+    res <- mainCheck "./problems/problem-spec.json" "./problems/solution-spec.json"
+    unless (res == 5343) do
+        error $ "Invalid spec score, expected 5343, got: " <> show res
 
 main :: IO ()
 main = do
-  getArgs >>= \case
-    [] -> mainSolve "./problems/problem-10.json"
-    ["test"] -> mainTest
-    ["render", problemPath] -> mainRender problemPath
-    ["solve", fp] -> mainSolve fp
-    ["check", problemPath, solutionPath] -> print =<< mainCheck problemPath solutionPath
-    _ -> error "usage: check pb solution | solve pb"
+    getArgs >>= \case
+        [] -> mainSolve "./problems/problem-10.json"
+        ["test"] -> mainTest
+        ["render", problemPath] -> mainRender problemPath
+        ["solve", fp] -> mainSolve fp
+        "save" : xs -> mapConcurrently_ saveSolve xs
+        ["check", problemPath, solutionPath] -> print =<< mainCheck problemPath solutionPath
+        _ -> error "usage: check pb solution | solve pb"
