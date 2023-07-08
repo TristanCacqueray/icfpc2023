61525dafdaeec92581696050378c432b013b7ecd
Jens Petersen <juhpetersen@gmail.com>
2023-07-09 02:51:25 +0800

    API: add accessAPI and use in userBoard

--- src/ProgCon/API.hs
+++ src/ProgCon/API.hs
@@ -1,33 +1,45 @@
 module ProgCon.API where
 
-import Data.Aeson
+import Data.Aeson (FromJSON)
 import Data.ByteString.Char8 qualified as B8
 import Network.HTTP.Client (applyBearerAuth)
 import Network.HTTP.Query
 import Network.HTTP.Simple
+import Network.HTTP.Types
 import System.Environment
 
 apiServer :: String
 apiServer = "https://api.icfpcontest.com"
 
+accessAPI :: FromJSON a => String -> Query -> (Request -> Request) -> Status
+          -> IO (Maybe a)
+accessAPI method params settings expected = do
+  token <- getEnv "ICFP_TOKEN"
+  withURLQuery (apiServer +/+ method) params $ \req -> do
+    response <-
+      httpJSON (applyBearerAuth (B8.pack token) $ settings req)
+    if getResponseStatus response == expected
+      then return $ Just $ getResponseBody response
+      else do
+      let status =  getResponseStatus response
+      putStrLn $ "status code: " ++ show (statusCode status) ++ " " ++ B8.unpack (statusMessage status)
+      --print (getResponseBody response :: Object)
+      return Nothing
+
 userBoard:: IO ()
 userBoard = do
-  token <- getEnv "ICFP_TOKEN"
-  withURLQuery (apiServer +/+ "userboard") [] $ \req -> do
-    eobj <- getResponseBody <$> httpJSONEither (applyBearerAuth (B8.pack token) req)
-    case eobj of
-      Left err -> print err
-      Right (obj :: Object) -> do
-        case lookupKey "Success" obj of
-          Nothing ->
-            case lookupKey "Failure" obj of
-              Just err -> putStrLn err
-              Nothing -> putStrLn $ "failed to get userboard: " ++ show obj
-          Just (ps :: Object) ->
-            case lookupKey "problems" ps of
-              Just (scores :: [Maybe Int]) ->
-                mapM_ renderScore (zip [(1::Int)..] scores)
-              Nothing -> putStrLn $ "no problems: " ++ show obj
+  mobj <- accessAPI "userboard" [] id status200
+  case mobj of
+    Nothing -> putStrLn "failed to get userboard"
+    Just obj -> do
+      -- FIXME combine with problems again
+      case lookupKey "Success" obj >>= lookupKey "problems" of
+        Just (scores :: [Maybe Int]) ->
+          mapM_ renderScore (zip [(1::Int)..] scores)
+        Nothing ->
+          case lookupKey "Failure" obj of
+            Just err -> putStrLn err
+            Nothing -> putStrLn $ "failed to get userboard: " ++ show obj
   where
     renderScore (pos,mscore) =
       putStrLn $ '#' : show pos ++ ": " ++ maybe "null" show mscore
